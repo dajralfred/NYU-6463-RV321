c00b0553245ce55aab4f17b9c0c4f0d64924f948
Shubham Shandilya <85631831+ss15590-NYU@users.noreply.github.com>
2021-11-23 18:42:46 -0500

    Updated after Dajr's inputs.

--- Shubham/REGISTER FILE/reg_file.vhd
+++ Shubham/REGISTER FILE/reg_file.vhd
@@ -24,51 +24,36 @@ USE	WORK.NYU_6463_RV32I_pkg.ALL;
 
 entity reg_file is
     Port (
-            CLK                                 : in std_logic;
-            ReadReg1, ReadReg2    : in std_logic_vector(4 downto 0);
+            CLK                  : in std_logic;
+            ReadReg1, ReadReg2   : in std_logic_vector(4 downto 0);
             ReadData1, ReadData2 : out std_logic_vector(31 downto 0);
-            WriteReg                        : in std_logic_vector(4 downto 0);
-            WriteData                      : in std_logic_vector(31 downto 0);
-            WriteEnable                   : in std_logic;
-            reg_out                          : out Register_Type
+            WriteReg             : in std_logic_vector(4 downto 0);
+            WriteData            : in std_logic_vector(31 downto 0);
+            WriteEnable          : in std_logic;
+            --reg_out              : out Register_Type -- to make the entire Statck register available to use.
     );
 end reg_file;
 
 architecture reg_file_body of reg_file is
 
-signal Reg32 : Register_Type := (
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000",
-    "00000000000000000000000000000000", "00000000000000000000000000000000"
-);
+signal Reg32 : Register_Type := (others => (others => '0'));
 
 begin
 
-    reg_out <= Reg32;
-    
-    -- write
+    --reg_out <= Reg32;
+
     process(CLK)
-    begin
-        if(rising_edge(CLK) AND WriteEnable = '1') then
-            Reg32(to_integer(unsigned(WriteReg))) <= WriteData;
-        end if;   
-    end process;
-    
-    -- read
-    ReadData1 <= Reg32(to_integer(unsigned(ReadReg1)));
-    ReadData2 <= Reg32(to_integer(unsigned(ReadReg2)));
+    begin        
+        if(rising_edge(CLK)) then
+            if (WriteEnable = '1') then
+                -- write
+                Reg32(to_integer(unsigned(WriteReg))) <= WriteData;
+            end if;
+         
+           -- read
+           ReadData1 <= Reg32(to_integer(unsigned(ReadReg1)));
+           ReadData2 <= Reg32(to_integer(unsigned(ReadReg2)));
+       end if;
+    end process;        
 
 end reg_file_body;
