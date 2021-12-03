4b8c00e44af4cd421e4d407fd3cf10ea4e9f0789
ArtSh1985 <94577245+ArtSh1985@users.noreply.github.com>
2021-12-02 21:50:51 -0500

    Add files via upload
    Ver 2.0

--- ARTEM/Imm_ext/Imm_ext.vhd
+++ ARTEM/Imm_ext/Imm_ext.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 11/26/2021 01:20:46 PM
+-- Create Date: 11/30/2021 07:10:13 PM
 -- Design Name: 
--- Module Name: Imm_ext - Behavioral
+-- Module Name: Imm_ext - Do_extension
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,7 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.OPCODES.all;
+use WORK.RV321_pkg.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -34,27 +34,37 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Imm_ext is
     Port (
-        imm_in: in std_logic_vector(31 downto 0);
-        imm_out: out std_logic_vector(31 downto 0);
-        sign_ext_imm: in std_logic;
-        opc_in: in std_logic_vector(6 downto 0)    
+        imm_in: in std_logic_vector(31 downto 0);   --32 bits immediate from the Control Unit
+        imm_out: out std_logic_vector(31 downto 0); --32 bits sign extended immetiate to the ALU
+        opc_in: in std_logic_vector(9 downto 0)     --10 bits funct3 & opcode from the Control Unit
     );
 end Imm_ext;
 
-architecture Behavioral of Imm_ext is
+architecture Do_extension of Imm_ext is
     
-    signal imm_12bit: std_logic_vector(11 downto 0);
-    signal imm_20bit: std_logic_vector(19 downto 0);
-    signal immediate: std_logic_vector(31 downto 0);
-   
+    signal imm_12bit: std_logic_vector(11 downto 0); --12 valid bits from imm_in
+    signal imm_20bit: std_logic_vector(19 downto 0); --20 valid bits from imm_in
+    signal immediate: std_logic_vector(31 downto 0); --vector for temporary storage sign extended immediate
+    signal prekey: std_logic_vector(9 downto 0);     --
+    signal sign_ext_imm: std_logic;
+    
 begin
-        
+    
+    prekey <= opc_in;
+    
+    with prekey select sign_ext_imm <= --sign_ext_imm assignment
+        '0' when "0000110111" | --LUI
+                 "0000010111" | --AUIPC
+                 "0010010011" | --SLLI
+                 "1010010011",  --SRLI and SRAI
+        '1' when others; 
+
     imm_12bit(11 downto 0) <= imm_in(11 downto 0);
     imm_20bit(19 downto 0) <= imm_in(19 downto 0);
         
     process(imm_in, opc_in, sign_ext_imm, immediate, imm_12bit, imm_20bit) begin
         
-        case opc_in is
+        case opc_in(6 downto 0) is
         
             when R_TYPE =>
                 immediate <= (others => '0');
@@ -115,4 +125,5 @@ begin
 
     imm_out <= immediate;
     
-end Behavioral;
+
+end Do_extension;
